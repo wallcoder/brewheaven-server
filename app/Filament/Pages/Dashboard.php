c126e7abf8a84b95c025b5127ce48512073a4129
biaka <biakropuia@lailen.com>
2025-10-06 17:29:49 +0530

    building panels

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -3,8 +3,8 @@
 namespace App\Filament\Pages;
 
 use Filament\Pages\Page;
-
-class Dashboard extends Page
+use Filament\Pages\Dashboard as BaseDashboard;
+class Dashboard extends BaseDashboard
 {
-    protected string $view = 'filament.pages.dashboard';
+    
 }
--- resources/views/logo.blade.php
+++ resources/views/logo.blade.php
@@ -1,3 +1,3 @@
 <div>
-    HomeBrew
+    BrewHeaven
 </div>
