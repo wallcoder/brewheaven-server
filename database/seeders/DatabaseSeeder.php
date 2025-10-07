338adf4cdcd510ebbbc38a7743136bb7a0e3ad84
validdepix@gmail.com <biaka>
2025-10-07 18:32:44 +0530

    stashing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,14 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $user = User::create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password')
         ]);
+
+        
+
+
     }
 }
